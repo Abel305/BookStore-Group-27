22b13814dc57679b38e538a0de97ebcbbd175d09
athom337 <122599151+athom337@users.noreply.github.com>
2023-04-14 05:50:19 -0500

    delete replaced feature

--- UserProfile/src/main/java/com/example/UserProfile/CreditCard/CreditCardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.UserProfile.CreditCard;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
-}
-
